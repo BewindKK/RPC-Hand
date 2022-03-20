fc92b58ce996a8238de6679487b2c3cd74bdeccf
bewind <70551899+BewindKK@users.noreply.github.com>
2022-03-20 15:31:07 +0800

    second commit

--- rpc-transport/src/main/java/com/bewind/transport/Impl/HTTPTransportClient.java
+++ rpc-transport/src/main/java/com/bewind/transport/Impl/HTTPTransportClient.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class HTTPTransportClient implements TransportClient {
 
